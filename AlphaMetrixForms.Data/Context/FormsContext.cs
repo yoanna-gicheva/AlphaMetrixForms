a37c31981fe6414c81b66874b2e9aef51eec9d4c
Yoanna Gicheva <32265059+yoanna777@users.noreply.github.com>
2020-05-10 18:44:25 +0300

    AlphaMetrixForms Initial Settings

---
+++ AlphaMetrixForms.Data/Context/FormsContext.cs
@@ -0,0 +1,13 @@
+﻿using AlphaMetrixForms.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlphaMetrixForms.Data.Context
+{
+    public class FormsContext : IdentityDbContext<User, Role, Guid>
+    {
+
+    }
+}
---
+++ AlphaMetrixForms.Data/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlphaMetrixForms.Data.Entities
+{
+    public class Role : IdentityRole<Guid>
+    {
+    }
+}
