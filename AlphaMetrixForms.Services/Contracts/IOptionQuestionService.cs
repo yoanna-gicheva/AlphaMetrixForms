9f4b46aa77f0e4cb88988ea8f36cba8517990da4
Yoanna Gicheva <32265059+yoanna777@users.noreply.github.com>
2020-05-14 02:55:05 +0300

    OptionQuestion service CRUD operations

---
+++ AlphaMetrixForms.Services/Contracts/IOptionQuestionService.cs
@@ -0,0 +1,17 @@
+﻿using AlphaMetrixForms.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaMetrixForms.Services.Contracts
+{
+    public interface IOptionsQuestionService
+    {
+        Task<OptionQuestionDTO> GetOptionsQuestionAsync(Guid questionId);
+        Task<OptionQuestionDTO> UpdateOptionsQuestionAsync(Guid questionId, OptionQuestionDTO questionDTO);
+        Task<OptionQuestionDTO> CreateOptionsQuestionAsync(OptionQuestionDTO questionDTO, Guid formId);
+        Task<ICollection<OptionQuestionDTO>> GetAllOptionsQuestionsAsync(Guid formId);
+        Task<bool> DeleteOptionsQuestionAsync(Guid questionId);
+    }
+}
