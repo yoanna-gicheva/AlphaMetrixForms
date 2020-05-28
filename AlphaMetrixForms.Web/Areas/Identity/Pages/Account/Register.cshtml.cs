5a9959e7004ee5d96b2ccc18fe49b952491a42a4
lachezar.bonchev <lachezar.bonchev@outlook.com>
2020-05-28 08:42:35 +0300

    Added login and registration with UserName

--- AlphaMetrixForms.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AlphaMetrixForms.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -50,6 +50,10 @@ namespace AlphaMetrixForms.Web.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
@@ -75,7 +79,7 @@ namespace AlphaMetrixForms.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
